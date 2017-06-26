9a97daae53dde1853134fb4f1d86800b37bf9a72
Luis.O.M <luismunoz.dh@gmail.com>
2017-06-26 17:06:34 -0300

    fullcalendar module running

--- src/app/dashboard/scheduler/scheduler.component.ts
+++ src/app/dashboard/scheduler/scheduler.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {CalendarComponent} from "ap-angular2-fullcalendar";
 
 @Component({
   selector: 'app-scheduler',
@@ -7,73 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduler.component.css']
 })
 export class SchedulerComponent implements OnInit {
+  
   calendarOptions:Object = {
-        height: 'parent',
+        //height: 'parent',
+        defaultView: 'agendaDay',
         fixedWeekCount : false,
-        defaultDate: '2016-09-12',
+        defaultDate: '2017-06-25',
         editable: true,
         eventLimit: true, // allow "more" link when too many events
         events: [
           {
             title: 'All Day Event',
-            start: '2016-09-01'
+            start: '2017-06-25'
           },
           {
             title: 'Long Event',
-            start: '2016-09-07',
-            end: '2016-09-10'
+            start: '2017-06-25',
+            end: '2017-06-26'
           },
           {
             id: 999,
             title: 'Repeating Event',
-            start: '2016-09-09T16:00:00'
+            start: '2017-06-25T16:00:00'
           },
           {
             id: 999,
             title: 'Repeating Event',
-            start: '2016-09-16T16:00:00'
+            start: '2017-06-25T16:00:00'
           },
           {
             title: 'Conference',
-            start: '2016-09-11',
-            end: '2016-09-13'
+            start: '2017-06-25',
+            end: '2017-06-27'
           },
           {
             title: 'Meeting',
-            start: '2016-09-12T10:30:00',
-            end: '2016-09-12T12:30:00'
-          },
-          {
-            title: 'Lunch',
-            start: '2016-09-12T12:00:00'
-          },
-          {
-            title: 'Meeting',
-            start: '2016-09-12T14:30:00'
-          },
-          {
-            title: 'Happy Hour',
-            start: '2016-09-12T17:30:00'
-          },
-          {
-            title: 'Dinner',
-            start: '2016-09-12T20:00:00'
-          },
-          {
-            title: 'Birthday Party',
-            start: '2016-09-13T07:00:00'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2016-09-28'
+            start: '2017-06-25T10:30:00',
+            end: '2017-06-25T12:30:00'
           }
+          
         ]
       };
 
   constructor() { }
 
   ngOnInit() {
+   
+  }
+
+    changeCalendarView(view) {
+   
   }
 
 }
