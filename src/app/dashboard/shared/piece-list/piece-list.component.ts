a56e264544f059fad4adc53084ff82b1cfd76494
Luis.O.M <luismunoz.dh@gmail.com>
2017-11-03 18:53:01 -0300

    Added search media + pagination

--- src/app/dashboard/shared/piece-list/piece-list.component.ts
+++ src/app/dashboard/shared/piece-list/piece-list.component.ts
@@ -3,65 +3,78 @@ import { PlayoutService } from '../../../_core/_services/playout.service'
 import { PieceModel } from '../../../_core/_models/piece.model';
 import { MediainfoComponent } from './mediainfo/mediainfo.component';
 declare var jQuery: any;
-declare var moment : any;
+declare var moment: any;
 
 /**
  * @author Luis Muñoz <luismunoz.dh@gmail.com>
  */
 @Component({
-  selector: 'mp-piece-list',
-  templateUrl: './piece-list.component.html',
-  styleUrls: ['./piece-list.component.css']
+    selector: 'mp-piece-list',
+    templateUrl: './piece-list.component.html',
+    styleUrls: ['./piece-list.component.css']
 })
 export class PieceListComponent implements AfterViewChecked {
+
+    @ViewChild(MediainfoComponent)
+    private mediaInfoPopup: MediainfoComponent;
+    @Input('isJuiDraggable') isJuiDraggable: boolean = false;
+
+    elementRef: ElementRef;
+    pieceFilteredList: Array<PieceModel> = new Array<PieceModel>();//result of a search
+    pieceLst: Array<PieceModel> = new Array<PieceModel>();// all pieces
   
-  @ViewChild(MediainfoComponent)
-  private mediaInfoPopup: MediainfoComponent;
-  @Input('isJuiDraggable')isJuiDraggable : boolean = false;
-  
-  elementRef: ElementRef;
-  pieceLst : Array<PieceModel> = new Array<PieceModel>();
+    constructor(private element: ElementRef, private playoutService: PlayoutService) {
+        this.elementRef = element;
+        this.playoutService.init();
+        this.playoutService.getPieceList().subscribe(resp => {
+            // Converts the duration to a printable format
+            resp.forEach(element => {
+                var mDur = moment.duration(element.duration, moment.ISO_8601);
+                element.durationHuman = moment.utc(mDur.asMilliseconds()).format("HH:mm:ss");
+            });
 
-  constructor(private element:ElementRef, private playoutService: PlayoutService){
-    this.elementRef = element;
-    this.playoutService.init();
-		this.playoutService.getPieceList().subscribe( resp  => {
-      // Converts the duration to a printable format
-      resp.forEach(element  => {
-        var mDur = moment.duration(element.duration, moment.ISO_8601);
-        element.durationHuman = moment.utc(mDur.asMilliseconds()).format("HH:mm:ss");
-      });
-
-			this.pieceLst = resp;  
-    });
-	}
-
-  
-  ngAfterViewChecked(){ // se ejecuta por cada evento/cambio en el dom.
-    if(this.isJuiDraggable){
-      let piecesLiDraggables = jQuery(".mp-item-media.list-group-item.draggable.ui-draggable.ui-draggable-handle");
-      let piecesLiElements = jQuery(".mp-item-media");
-
-      if(piecesLiElements .length != piecesLiDraggables.length){
-          
-          for(var i = 0; i <= piecesLiElements .length -1; i++){
-            jQuery(piecesLiElements[i]).draggable({revert:true,zIndex:999,revertDuration:100});
-
-            jQuery(piecesLiElements[i]).data('pieceData', {
-                title: this.pieceLst[i].name, // use the element's text as the event title
-                stick: true, // maintain when user navigates (see docs on the renderEvent method)
-                piece : this.pieceLst[i]
-              });
-          }
-      }
+            this.pieceLst = resp;
+            this.pieceFilteredList = resp;
+        });
     }
+
+
+    ngAfterViewChecked() { // se ejecuta por cada evento/cambio en el dom.
+        if (this.isJuiDraggable) {
+            let piecesLiDraggables = jQuery(".mp-item-media.list-group-item.draggable.ui-draggable.ui-draggable-handle");
+            let piecesLiElements = jQuery(".mp-item-media");
+
+            if (piecesLiElements.length != piecesLiDraggables.length) {
+
+                for (var i = 0; i <= piecesLiElements.length - 1; i++) {
+                    jQuery(piecesLiElements[i]).draggable({ revert: true, zIndex: 999, revertDuration: 100 });
+
+                    jQuery(piecesLiElements[i]).data('pieceData', {
+                        title: this.pieceLst[i].name, // use the element's text as the event title
+                        stick: true, // maintain when user navigates (see docs on the renderEvent method)
+                        piece: this.pieceLst[i]
+                    });
+                }
+            }
+        }
+
+
+    }
+
+    onClickMediaInfo(piece: PieceModel) {
+        this.mediaInfoPopup.show(piece.media);
+    }
+
+    onSearch(event) {
+        const val = event.target.value.toLowerCase();
+        // filter our data
+        this.pieceFilteredList = this.pieceLst.filter(function (p) {
+            return p.name.toLowerCase().indexOf(val) !== -1 || !val;
+        });
     
-
-  }
-
-  onClickMediaInfo(piece: PieceModel){
-		console.log(piece);
-		this.mediaInfoPopup.show(piece.media);
-	}
+        if(val == ""){
+            this.pieceFilteredList = this.pieceLst;
+        }
+    }
 
 }
--- src/app/dashboard/shared/piece-list/piece-list.module.ts
+++ src/app/dashboard/shared/piece-list/piece-list.module.ts
@@ -4,6 +4,7 @@ import {PieceListComponent} from './piece-list.component';
 import {DndModule} from 'ng2-dnd';
 //import {PopupModule} from 'ng2-opd-popup';
 import { MediainfoComponent } from './mediainfo/mediainfo.component';
+import {NgxPaginationModule} from 'ngx-pagination'; 
 
 /**
 * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -13,6 +14,8 @@ import { MediainfoComponent } from './mediainfo/mediainfo.component';
     imports: [
         CommonModule, 
         DndModule.forRoot(),
+        NgxPaginationModule
+
         //PopupModule.forRoot(),
     ],
     declarations: [PieceListComponent, MediainfoComponent],
